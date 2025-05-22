9d2ddfa46fa5ddfc51cfae3f08dbbb219fb6d870
Siim Pihlak <info@simplist.digital>
2025-05-23 01:10:07 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const multiplicationTable = document.getElementById("multiplication-table");
+
+for (let row = 1; row <= 10; row++) {
+    for (let col = 1; col <= 10; col++) {
+        const cell = document.createElement("div");
+        cell.textContent = row * col;
+        multiplicationTable.appendChild(cell);
+        if (col == 1 || row == 1) {
+            cell.classList.add("header");
+        }
+        if (col / row == 1) {
+            cell.classList.add("product");
+        }
+    }
+    
+}
